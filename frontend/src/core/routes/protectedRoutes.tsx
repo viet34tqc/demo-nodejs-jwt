addc8afb9f00957291da420963a25d99b35c6a1d
viet34tqc <viet34tqc@gmail.com>
2023-03-11 16:58:22 +0700

    Fix route

--- frontend/src/core/routes/protectedRoutes.tsx
+++ frontend/src/core/routes/protectedRoutes.tsx
@@ -1,6 +1,6 @@
 import loadable from '@loadable/component'
 const ProtectedRoute = loadable(() => import('./components/ProtectedRoute'))
-const Posts = loadable(() => import('@/views/SinglePost/components'))
+const Posts = loadable(() => import('@/views/Posts'))
 const SinglePost = loadable(() => import('@/views/SinglePost'))
 const Profile = loadable(() => import('@/views/Profile'))
 
